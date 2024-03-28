6661a0822a9a6c53d51041e2b702ac9a410e60a0
Sarah Risma <sarahgrace.risma@calbaptist.edu>
2024-03-27 23:31:29 -0700

    fixed up code and got it to output something!

--- Raytracer.java
+++ Raytracer.java
@@ -65,15 +65,20 @@ public class Raytracer {
 
         // 4: Determine whether a line and triangle intersect
         for (int i = 0; i < faces.size(); i += 3) {
-            Vector3D V0 = vertices.get(i);
-            Vector3D V1 = vertices.get(i + 1);
-            Vector3D V2 = vertices.get(i + 2);
+            int[] face = faces.get(i);
+            Vector3D V0 = vertices.get(face[0]); // vertices.get(i);
+            Vector3D V1 = vertices.get(face[1]); // vertices.get(i + 1);
+            Vector3D V2 = vertices.get(face[2]); // vertices.get(i + 2);
 
             // check for intersection between line and triangle
             boolean intersects = intersects(lineStart, lineEnd, V0, V1, V2);
 
             if (intersects) {
                 System.out.println("Intersection detected with triangle " + (i / 3));
+            }
+
+            if( (i + 3) >= faces.size() - 3) {
+                break;
             }
         }
 
@@ -99,7 +104,7 @@ public class Raytracer {
         System.out.println("Angle between the two lines: " + angle + " degrees");
 
         // 8: Compute the illumination for a triangle
-        BufferedImage image = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB); // TYPE_INT_ARGB
         double intensity = computeLambertianIntensity(normal); // Compute intensity based on Lambertian shading
         Color illuminatedColor = applyLightIntensity(SURFACE_COLOR, intensity); // Apply light color and intensity to the surface color
         drawTriangle(image, vertex1, vertex2, vertex3, illuminatedColor); // Draw triangle with illuminated color
@@ -108,7 +113,7 @@ public class Raytracer {
         BufferedImage obj2D = raytraceObjTo2D(vertices);
 
         // 10: Output screen pixels as an image
-//        exportPNG();
+        exportPNG(obj2D);
     }
 
     //==================================================================================================================
@@ -417,8 +422,11 @@ public class Raytracer {
             Vector3D vertex = vertices.get(i);
             double x = vertex.x;
             double y = vertex.y;
-            int color = Integer.parseInt(SURFACE_COLOR.toString());
-            ret.setRGB((int)x, (int) y, color);
+            String color = SURFACE_COLOR.toString();
+            int equalsIndex = color.indexOf("=") + 1;
+            String rgb = color.substring(equalsIndex, equalsIndex + 3);
+            int colorToInt = 65536 * Integer.parseInt(rgb) + 256 * 0 + 0; // Integer.parseInt(rgb);
+            ret.setRGB((int)x, (int) y, colorToInt);
         }
 
         return ret;
@@ -426,41 +434,23 @@ public class Raytracer {
 
     //==================================================================================================================
     //TODO ========== 10: Output screen pixels as an image ==========
-    public static void exportPNG() {
+    public static void exportPNG(BufferedImage input) {
         // width of the image
-        int width = 963;
+        int width = IMAGE_WIDTH;
         // height of the image
-        int height = 640;
-        // For storing image in RAM
-        BufferedImage image = null;
-        // READ IMAGE
-        try {
-            File input_file = new File("Goomba.png");
-            // image file path create an object of
-            // BufferedImage type and pass as parameter the
-            // width,  height and image int
-            // type. TYPE_INT_ARGB means that we are
-            // representing the Alpha , Red, Green and Blue
-            // component of the image pixel using 8 bit
-            // integer value.
-            image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-            // Reading input file
-            image = ImageIO.read(input_file);
-            System.out.println("Reading complete.");
-        }
-        catch (IOException e) {
-            System.out.println("Error: " + e);
-        }
+        int height = IMAGE_HEIGHT;
+        System.out.println("Reading complete.");
         // WRITE IMAGE
         try {
             // Output file path
             File output_file = new File("output.png");
             // Writing to file taking type and path as
-            ImageIO.write(image, "png", output_file);
+            ImageIO.write(input, "png", output_file);
             System.out.println("Writing complete.");
         }
         catch (IOException e) {
             System.out.println("Error: " + e);
         }
     }
+
 }
